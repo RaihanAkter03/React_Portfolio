62406a712a2455d7c60c0a0e0e2e627bdff3fc02
RaihanAkter03 <raihanakter26@gmail.com>
2021-02-11 20:24:52 -0600

    changes to the files

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,39 +1,44 @@
 import React, { Component } from 'react';
-export default class Header extends Component {
-render() {
-    let resumeData = this.props.resumeData;
-    return (
-        <React.Fragment>
 
+class Header extends Component {
+    render() {
+
+        if (this.props.data) {
+            var name = this.props.data.name;
+            var occupation = this.props.data.occupation;
+            var description = this.props.data.description;
+            var city = this.props.data.address.city;
+            var networks = this.props.data.social.map(function (network) {
+                return <li key={network.name}><a href={network.url}><i className={network.className}></i></a></li>
+            })
+        }
+
+        return (
             <header id="home">
+
                 <nav id="nav-wrap">
+
                     <a className="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
-                    <a className="mobile-btn" href="#" title="Hide navigation">Hide navigation</a>
+                    <a className="mobile-btn" href="#home" title="Hide navigation">Hide navigation</a>
+
                     <ul id="nav" className="nav">
                         <li className="current"><a className="smoothscroll" href="#home">Home</a></li>
                         <li><a className="smoothscroll" href="#about">About</a></li>
+                        <li><a className="smoothscroll" href="#resume">Resume</a></li>
                         <li><a className="smoothscroll" href="#portfolio">Portfolio</a></li>
+                        {/* <li><a className="smoothscroll" href="#testimonials">Testimonials</a></li> */}
                         <li><a className="smoothscroll" href="#contact">Contact</a></li>
                     </ul>
+
                 </nav>
 
                 <div className="row banner">
                     <div className="banner-text">
-                        <h1 className="responsive-headline">I am {resumeData.name}.</h1>
-                        <h3 style={{ color: '#fff', fontFamily: 'sans-serif ' }}>I am a {resumeData.role}.{resumeData.roleDescription}
-                        </h3>
+                        <h1 className="responsive-headline">I'm {name}.</h1>
+                        <h3>I'm a {city} based <span>{occupation}</span>. {description}.</h3>
                         <hr />
                         <ul className="social">
-                            {
-                                resumeData.socialLinks && resumeData.socialLinks.map(item => {
-                                    return (
-                                        <li key={item.name}>
-                                        <a href={item.url} target="_blank"><i className={item.className}></i></a>
-                                        </li>
-                                        )
-                                    }
-                                )
-                            }
+                            {networks}
                         </ul>
                     </div>
                 </div>
@@ -43,7 +48,8 @@ render() {
                 </p>
 
             </header>
-        </React.Fragment>
-    );
+        );
     }
 }
+
+export default Header;
