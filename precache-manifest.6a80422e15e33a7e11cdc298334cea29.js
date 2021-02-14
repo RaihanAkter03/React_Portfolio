b5ec94031627c3ef12a0ccdd4fa989a3931fa091
RaihanAkter03 <raihanakter26@gmail.com>
2021-02-13 19:38:22 -0600

    Updates

---
+++ precache-manifest.6a80422e15e33a7e11cdc298334cea29.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45805c53ab15b7c867683163570e895b",
+    "url": "/RaihanAkter03/React_Portfolio/index.html"
+  },
+  {
+    "revision": "0729e823ebec8e3792f8",
+    "url": "/RaihanAkter03/React_Portfolio/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "72e6aa25e720afac21a1",
+    "url": "/RaihanAkter03/React_Portfolio/static/js/2.0e9c2b2a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/RaihanAkter03/React_Portfolio/static/js/2.0e9c2b2a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0729e823ebec8e3792f8",
+    "url": "/RaihanAkter03/React_Portfolio/static/js/main.6e7d841b.chunk.js"
+  },
+  {
+    "revision": "734428276591eb824208",
+    "url": "/RaihanAkter03/React_Portfolio/static/js/runtime-main.6ce53423.js"
+  }
+]);
