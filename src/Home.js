534e3c194e843362844f2363abdff32f142eb927
RaihanAkter03 <raihanakter26@gmail.com>
2021-02-13 20:52:25 -0600

    added application photo

--- src/Home.js
+++ src/Home.js
@@ -15,7 +15,7 @@ export const Home = () => (
       <Styles>
       <Row>  
       <Col md={12} sm={8} smOffset={2}>
-        <Image src="assets/images/profile.jpg" height="200px" width="200px" className="about-profile-pic" rounded />
+        <Image src="../assets/images/profile.jpg" height="200px" width="200px" className="about-profile-pic" rounded />
         <p>Skilled software developer in the industrial supplier space. Experienced in all stages of software development and project management. Well-versed in current programming languages, techniques and best practices. Deep knowledge of all elements of business: supervision, product management, finance, customer acquisition and customer service.</p>
       </Col>
      </Row> 
