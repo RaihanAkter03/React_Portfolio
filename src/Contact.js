11a20970b6a3f346a26ce0e77d730a60cbc73718
RaihanAkter03 <raihanakter26@gmail.com>
2021-02-15 15:39:59 -0600

    modify resume part

--- src/Contact.js
+++ src/Contact.js
@@ -1,5 +1,6 @@
 import  React from "react";
 import { ListGroup } from 'react-bootstrap';
+import resume from "./assets/RaihanAkter.docx"
 
 export const Contact = () => (
     <div>
@@ -8,7 +9,7 @@ export const Contact = () => (
             <ListGroup.Item action href="https://github.com/RaihanAkter03" target="_blank"> <i class="fab fa-github-square"></i>  Github</ListGroup.Item>
             <ListGroup.Item action href="https://www.linkedin.com/in/raihan-akter91" target="_blank">  <i class="fab fa-linkedin"></i>  Linkedin</ListGroup.Item>
             <ListGroup.Item action href="https://github.com/RaihanAkter03/React_Portfolio" target="_blank"> <i class="fas fa-laptop"></i> WebSite</ListGroup.Item>
-            <ListGroup.Item action href="assets/RaihanAkter.docx" target="_blank"> <i class="fas fa-file"></i> Resume </ListGroup.Item>
+            <ListGroup.Item action href={resume} target="_blank"> <i class="fas fa-file"></i> Resume </ListGroup.Item>
         </ListGroup>
     
      </div>
